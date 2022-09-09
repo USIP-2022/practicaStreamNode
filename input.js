62a632c08a69a34fa4bb166bb5bb2202f26b4391
yacel100 <master.yac@gmail.com>
2022-09-09 19:32:23 -0400

    Subiendo la documentacion del proyecto

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-const fileStream = fs.createReadStream("./data/file.txt");
+const fileStream = fs.createReadStream("./data/testdata.txt");
 fileStream
   .on("data", (data) => {
     console.log("Leyendo los datos:", data.toString());
   })
-  .on("end", () => { console.log("No se tiene mas datos."); });
+  .on("end", () => { console.log("No se tiene  datos."); });
